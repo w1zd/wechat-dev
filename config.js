8d1fb7ba1bc998f632a47d897cce6c82fc5ffca0
Ryan <avakpan@gmail.com>
2018-01-10 22:34:34 +0800

    add all new files

---
+++ config.js
@@ -0,0 +1,8 @@
+self.$config = {
+    nav: [
+      {title: '概述', path: '/'},
+      {title: '接口说明', path: '/interface'},
+      {title: '聊天机器人', path: '/chatrobot'}
+    ],
+    tocVisibleDepth: 2
+  }
