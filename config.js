848669f6588fc391924967bb79e029c6c5dc3915
Ryan <panming@itcast.cn>
2018-01-25 03:58:13 +0800

    添加了关于access_token的说明

--- config.js
+++ config.js
@@ -5,6 +5,7 @@ self.$config = {
       {title: '聊天机器人', path: '/chatrobot'},
       {title: '微信网页开发', path: '/page'},
       {title: 'JS-SDK列表', path: '/jssdklist'},
+      {title: 'access_token说明', path: '/accesstoken'},
       {title: '附录', path: '/appendix'}
     ],
     tocVisibleDepth: 2
